91278a8252c5edfd853f4921a522c4c6b283cf9b
Luke Jordan <luke.jordan@nerdery.com>
2023-12-10 15:17:06 -0600

    First working draft (beta)
    Produces a table of keywords counts in each region
    Only searches the keywords property, not description
    e.g. Finds Barrier on Shen, but not references to Barrier on his support cards

--- lor_keywords.py
+++ lor_keywords.py
@@ -3,34 +3,6 @@ from pathlib import Path
 import json
 import pandas as pd
 
-KW_COLUMNS = (
-    'Keyword',
-    'BC',
-    'BW',
-    'DE',
-    'FR',
-    'IO',
-    'MT',
-    'NX',
-    'PZ',
-    'SH',
-    'SI'
-)
-
-VOCAB_COLUMNS = (
-    'Vocab',
-    'BC',
-    'BW',
-    'DE',
-    'FR',
-    'IO',
-    'MT',
-    'NX',
-    'PZ',
-    'SH',
-    'SI'
-)
-
 DB_COLUMNS = (
     'cardCode',
     'collectible',
@@ -51,53 +23,70 @@ DB_COLUMNS = (
     'set'
 )
 
-if __name__ == '__main__':
-    # Todo: function to download set data and extract it (optionally)
-    # function to load sets
-    card_sets = [x for x in Path('data').glob('set1*.json') if x.is_file()]
+def card_sets_df(filename_pattern):
+    """
+    return card sets matching filename_pattern in the data directory as a dataframe
+    """
+    set_files = [x for x in Path('data').glob(filename_pattern) if x.is_file()]
     sets = list()
-    for card_set in card_sets:
-        sets.append(pd.read_json(card_set))
-    sets = pd.concat(sets)
-    sets_kwr = pd.DataFrame(sets, columns=('cardCode','regions','keywords'))
-    # convert region names to abbreviated names
-    # multiple region cards count in both regions
-    # print(sets)
-    # sets.to_csv('database.csv', columns=DB_COLUMNS)
+    for s in set_files:
+        sets.append(pd.read_json(s))
+    return pd.concat(sets)
 
-    # From this dataset, I just want a map:
-    # region : { keyword : count },
-    # region : ...
+
+def globals_json():
+    """
+    All globals. Each must be extracted to its own dataframe.
+    """
     with open("data/globals-en_us.json", "r") as gd_file:
         global_data = json.load(gd_file)
-
-    # vocab = pd.DataFrame(global_data['vocabTerms'])
-    # vocab_table = pd.DataFrame(columns=VOCAB_COLUMNS)
-    # vocab_table['Vocab'] = vocab['name']
-    # print(vocab_table)
-
-    keywords = pd.DataFrame(global_data['keywords'])
-    keyword_table = pd.DataFrame(columns=KW_COLUMNS)
-    keyword_table[KW_COLUMNS[0]] = keywords['name']
-
-    print(keyword_table)
-    print(sets_kwr)
-
-    print(f"1-region cards {len(sets_kwr[sets_kwr['regions'].apply(lambda x: len(x) == 1)])}")
-    print(f"2-region cards {len(sets_kwr[sets_kwr['regions'].apply(lambda x: len(x) == 2)])}")
-
-    print(f"1-keywords cards {len(sets_kwr[sets_kwr['keywords'].apply(lambda x: len(x) == 0)])}")
-    print(f"2-keywords cards {len(sets_kwr[sets_kwr['keywords'].apply(lambda x: len(x) == 1)])}")
-    print(f"3-keywords cards {len(sets_kwr[sets_kwr['keywords'].apply(lambda x: len(x) == 2)])}")
-    print(f"4-keywords cards {len(sets_kwr[sets_kwr['keywords'].apply(lambda x: len(x) == 3)])}")
+    # print(global_data)
+    return global_data
 
 
-    sets_kwr['regions'] = sets_kwr['regions'].apply(lambda x : x if isinstance(x, list) else [x])
-    sets_kwr['keywords'] = sets_kwr['keywords'].apply(lambda x : x if isinstance(x, list) else [x])
-    sets_exploded = sets_kwr.explode('regions').explode('keywords')
-    print(sets_exploded)
+def region_nm_abbr_map(global_data):
+    """
+    return dict of name to abbreviation region mapping
+    """
+    r_map = pd.DataFrame(global_data['regions'], columns=('name', 'abbreviation'))
+    r_map = r_map.set_index('name')['abbreviation'].to_dict()
+    # print(r_map)
+    return r_map
 
-    sets_counts = sets_exploded.groupby(['regions', 'keywords']).size().reset_index(name='Counts')
-    print(sets_counts)
-    sets_counts.to_csv('kwxregion.csv')
 
+def kw_count_by_region(sets_df, global_data):
+    """
+    return a dataframe of keywords and their frequency in each region
+    """
+    cards_with_kw_and_reg = pd.DataFrame(sets_df, columns=('cardCode','regions','keywords'))
+    # print(cards_with_kw_and_reg)
+    # print(cards_with_kw_and_reg['regions'])
+    # print(cards_with_kw_and_reg['keywords'])
+    kw_and_reg_exploded = cards_with_kw_and_reg.explode('regions').explode('keywords')
+    kw_and_reg_exploded = kw_and_reg_exploded.fillna("None")
+    # print(kw_and_reg_exploded)
+
+    kw_reg_count = kw_and_reg_exploded.groupby(['regions', 'keywords']).size().reset_index(name='Count')
+    kw_reg_count['regions'] = kw_reg_count['regions'].replace(region_nm_abbr_map(global_data))
+    # print(kw_reg_count)
+
+    regions = kw_reg_count['regions'].unique()
+    keywords = kw_reg_count['keywords'].unique()
+    final_result_frame = pd.DataFrame([(r, k) for r in regions for k in keywords], columns=['regions', 'keywords'])
+    result = pd.merge(final_result_frame, kw_reg_count, on=['regions', 'keywords'], how='left')
+    result['Count'] = result['Count'].fillna(0).astype(int)
+    # print(result)
+    return result
+
+
+# todo: function to download set data and extract it
+# todo: look in description for keywords, not just the keywords list
+# todo: filter by only cards in standard
+
+if __name__ == '__main__':
+
+    sets = card_sets_df('set*.json')
+    global_data = globals_json()
+
+    keywords_by_region = kw_count_by_region(sets, global_data)
+    keywords_by_region.to_csv('keywords_by_region.csv')
