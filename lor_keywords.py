d6e1e0579ada288fe56622eee5cc2c5fd1b3a028
Luke Jordan <luke.jordan@nerdery.com>
2023-12-09 23:18:54 -0600

    Counts keywords per region for set 1 ('keywords' field only, not description

--- lor_keywords.py
+++ lor_keywords.py
@@ -54,27 +54,50 @@ DB_COLUMNS = (
 if __name__ == '__main__':
     # Todo: function to download set data and extract it (optionally)
     # function to load sets
-    card_sets = [x for x in Path('data').glob('set*.json') if x.is_file()]
+    card_sets = [x for x in Path('data').glob('set1*.json') if x.is_file()]
     sets = list()
     for card_set in card_sets:
         sets.append(pd.read_json(card_set))
     sets = pd.concat(sets)
+    sets_kwr = pd.DataFrame(sets, columns=('cardCode','regions','keywords'))
     # convert region names to abbreviated names
     # multiple region cards count in both regions
     # print(sets)
     # sets.to_csv('database.csv', columns=DB_COLUMNS)
 
     # From this dataset, I just want a map:
-    # region : {keyword : (maindeck_count, generated_count), keyword...}
+    # region : { keyword : count },
+    # region : ...
     with open("data/globals-en_us.json", "r") as gd_file:
         global_data = json.load(gd_file)
 
-    vocab = pd.DataFrame(global_data['vocabTerms'])
-    keywords = pd.DataFrame(global_data['keywords'])
-    vocab_table = pd.DataFrame(columns=VOCAB_COLUMNS)
-    vocab_table['Vocab'] = vocab['name']
-    keyword_table = pd.DataFrame(columns=KW_COLUMNS)
-    keyword_table['Keyword'] = keywords['name']
+    # vocab = pd.DataFrame(global_data['vocabTerms'])
+    # vocab_table = pd.DataFrame(columns=VOCAB_COLUMNS)
+    # vocab_table['Vocab'] = vocab['name']
+    # print(vocab_table)
 
-    print(vocab_table)
+    keywords = pd.DataFrame(global_data['keywords'])
+    keyword_table = pd.DataFrame(columns=KW_COLUMNS)
+    keyword_table[KW_COLUMNS[0]] = keywords['name']
+
     print(keyword_table)
+    print(sets_kwr)
+
+    print(f"1-region cards {len(sets_kwr[sets_kwr['regions'].apply(lambda x: len(x) == 1)])}")
+    print(f"2-region cards {len(sets_kwr[sets_kwr['regions'].apply(lambda x: len(x) == 2)])}")
+
+    print(f"1-keywords cards {len(sets_kwr[sets_kwr['keywords'].apply(lambda x: len(x) == 0)])}")
+    print(f"2-keywords cards {len(sets_kwr[sets_kwr['keywords'].apply(lambda x: len(x) == 1)])}")
+    print(f"3-keywords cards {len(sets_kwr[sets_kwr['keywords'].apply(lambda x: len(x) == 2)])}")
+    print(f"4-keywords cards {len(sets_kwr[sets_kwr['keywords'].apply(lambda x: len(x) == 3)])}")
+
+
+    sets_kwr['regions'] = sets_kwr['regions'].apply(lambda x : x if isinstance(x, list) else [x])
+    sets_kwr['keywords'] = sets_kwr['keywords'].apply(lambda x : x if isinstance(x, list) else [x])
+    sets_exploded = sets_kwr.explode('regions').explode('keywords')
+    print(sets_exploded)
+
+    sets_counts = sets_exploded.groupby(['regions', 'keywords']).size().reset_index(name='Counts')
+    print(sets_counts)
+    sets_counts.to_csv('kwxregion.csv')
+
