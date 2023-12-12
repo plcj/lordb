811ef95babb9b243eeb731395f8a6ae2200a77b0
Luke Jordan <luke.jordan@nerdery.com>
2023-12-12 12:19:56 -0600

    Normalize format across cards. Add format to database.

--- lor_keywords.py
+++ lor_keywords.py
@@ -13,7 +13,7 @@ def card_sets_df(filename_pattern):
     all_sets = list()
     for s in set_files:
         all_sets.append(pd.read_json(s))
-    return pd.concat(all_sets)
+    return pd.concat(all_sets).reset_index(drop=True) # reset_index to avoid duplicate indices
 
 
 def globals_json():
@@ -61,14 +61,40 @@ def region_abbreviation_map(lor_globals):
     return r_map
 
 
-# todo preprocess card data to make format consistent across associated cards
+def normalize_format(cards):
+    """
+    This function prevents cards that aren't in Standard from being part of our results.
+
+    return a dataframe of cards with consistent formats across related cards.
+
+    Related can mean associated, but not all related cards are part of associated data, e.g. Twin Disciplines.
+    When cards aren't in Standard, their related cards appear not to have been modified to exclude the format.
+    """
+    normalized_cards = cards.copy()
+    normalized_cards['baseCardCode'] = normalized_cards['cardCode'].str.replace(r'T\d$', '', regex=True)
+    grouped_cards = normalized_cards.groupby('baseCardCode')
+    for name, group in grouped_cards:
+        base_card = group[group['cardCode'] == name]
+        if not base_card.empty and 'Standard' not in base_card['formats'].values[0]:
+            # if base card doesn't have 'Standard' format, related cards shouldn't either
+            for idx, row in group.iterrows():
+                formats = row['formats']
+                if'Standard' in formats:
+                    formats_copy = formats.copy()
+                    formats_copy.remove('Standard')
+                    normalized_cards.loc[idx, 'formats'] = formats_copy
+    # normalized_cards.to_csv('normalized_cards.csv')
+
+    return normalized_cards
+
 
 def kw_count_by_region(sets_df, lor_globals):
     """
     return a dataframe of keywords and their frequency in each region
     """
+    # todo move preprocessing to separate functions
     # Get cards. Limit to those in Standard
-    cards = pd.DataFrame(sets_df, columns=['cardCode', 'formats', 'regionRefs', 'keywordRefs', 'description'])
+    cards = pd.DataFrame(sets_df, columns=['cardCode', 'name', 'collectible', 'formats', 'regionRefs', 'keywordRefs', 'description'])
     standard_cards = cards[cards['formats'].apply(lambda formats: isinstance(formats, list) and 'Standard' in formats)].copy()
     ## Why isinstance(formats, list)? https://github.com/RiotGames/developer-relations/issues/785 When not present, pandas makes it NaN, which internally is a float
 
@@ -81,6 +107,7 @@ def kw_count_by_region(sets_df, lor_globals):
     standard_cards = standard_cards.fillna("None")  # not all cards have keywords, and we don't want them showing up as NaN
     standard_cards = standard_cards.query('regionRefs != "Runeterra"')  # Remove Runeterra region
     standard_cards = standard_cards.replace(region_abbreviation_map(lor_globals)) # Replace region names with abbreviations
+    standard_cards.to_csv('keywords_by_region_debug.csv')
 
     ## Count keywords by region
     kw_by_reg_with_count = standard_cards.groupby(['regionRefs', 'all_keywords']).size().reset_index(name='Count')
@@ -97,6 +124,7 @@ def kw_count_by_region(sets_df, lor_globals):
 
 if __name__ == '__main__':
     sets = card_sets_df('set*.json')
+    sets = normalize_format(sets)
     global_data = globals_json()
 
     keywords_by_region = kw_count_by_region(sets, global_data)
